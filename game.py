0d7f1fe223ab02b35f498cf6dded216694d1dcd2
Austin Ward <wardaustin2001@gmail.com>
2025-11-23 17:00:56 -0500

    initial window

---
+++ game.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Pointer Accuracy Game")
+    root.geometry("800x600")
+    root.config(bg="white")
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
